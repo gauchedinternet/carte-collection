86c6edded8aa073fef7a9c2dfd9bcef7b143f5d8
Tom Chauvel <tom.chauvel@proton.me>
2024-06-19 17:56:06 +0200

    maj

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,25 +3,37 @@
     import { afterUpdate, onMount } from "svelte";
 
     import { Template, CE_Text, CE_Picture, CE_Vec2, CE_FormattedText } from "canvas-editor";
-    import type { configuration } from "canvas-editor";
 
     import Input from "$lib/Input.svelte";
+
+    let template:Template = new Template(new CE_Vec2(744,1039))
+
+    let files1: File[]
+    let image1 = new CE_Picture()
+    image1.position = new CE_Vec2(70,122)
+    image1.resize(new CE_Vec2(600,394))
+    template.add(image1)
+
+    let background = new CE_Picture()
+    template.add(background)
 
     let nom:CE_Text = new CE_Text('Impact',33,"400","#aa2c4a",0,"left")
     nom.data = "Rachel Kéké (50ans)"
     nom.position = new CE_Vec2(71,68)
+    template.add(nom)
 
     let circo:CE_Text = new CE_Text('Impact',20.7,"400","#aa2c4a",0,"left")
     circo.data = "7e circonscription du Val-de-Marne"
     circo.position = new CE_Vec2(71,96)
+    template.add(circo)
 
     let credit:CE_Text = new CE_Text('Impact',20.7,"400","#a48199",0,"left")
     credit.position = new CE_Vec2(80,541)
+    template.add(credit)
     
-    let partie:string = ""
-
     let att1_titre:CE_Text = new CE_Text('Impact',33,"400","#aa2c4a",0,"left")
     att1_titre.position = new CE_Vec2(70,585)
+    template.add(att1_titre)
 
     let f1 = (input:string) => {
         return {
@@ -37,60 +49,31 @@
     }
     let att1_desc:CE_Text = new CE_FormattedText(f1,670)
     att1_desc.position = new CE_Vec2(70,619)
+    template.add(att1_desc)
 
     let att2_titre:CE_Text = new CE_Text('Impact',25,"400","#f2ac9a",0,"left")
     att2_titre.position = new CE_Vec2(70,702)
+    template.add(att2_titre)
 
     let att2_desc:CE_Text = new CE_Text('Impact',33,"400","#ded6c4",0,"left")
     att2_desc.position = new CE_Vec2(71,744)
     att2_desc.width = 480
+    template.add(att2_desc)
 
     let att3_titre:CE_Text = new CE_Text('Impact',25,"400","#f2ac9a",0,"left")
     att3_titre.position = new CE_Vec2(70,804)
+    template.add(att3_titre)
 
     let att3_desc:CE_Text = new CE_FormattedText(f1,670)
     att3_desc.position = new CE_Vec2(70,833)
+    template.add(att3_desc)
 
+    let partie:string = ""
     let loi_image = new CE_Picture()
     let loi :string="base"
+    template.add(loi_image)
 
-    let background = new CE_Picture()
-
-    let files1: File[]
-    let image1 = new CE_Picture()
-    image1.position = new CE_Vec2(70,122)
-    image1.resize(new CE_Vec2(600,394))
-
-    let canvas : HTMLCanvasElement|undefined = undefined
-
-    let template:Template
-    let config:configuration
-
-
-    onMount(() => {
-        config = {
-            height:1039,
-            width:744,
-            canvas:canvas!
-        }
-        template = new Template(config)
-        
-        template.add(image1)
-        template.add(background)
-
-        template.add(nom)
-        template.add(circo)
-        template.add(credit)
-        template.add(att1_titre)
-        template.add(att1_desc)
-        template.add(att2_titre)
-
-
-        template.add(att2_desc)
-        template.add(att3_titre)
-        template.add(att3_desc)
-
-        template.add(loi_image)
+    onMount(() => {        
         loop()
     })
 
@@ -107,6 +90,7 @@
     })
 
     function loop(){
+        if (template.destroyed) {return}
         template.draw()
         requestAnimationFrame(loop);
     }
@@ -117,7 +101,7 @@
 
 <div class="relative grid gap-4 grid-cols-2 p-8 h-screen w-screen" spellcheck="false">
 
-    <canvas class="h-full max-h-screen place-self-center border border-2" bind:this={canvas}></canvas>
+    <canvas class="h-full max-h-screen place-self-center border border-2" bind:this={template.canvas}></canvas>
 
     <div class="flex gap-4 flex-col overflow-y-auto overflow-x-visible">
 
